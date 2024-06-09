782e794de4d8df4c8ea509de9b0c08667ad1a2c0
Awounfouet Akeufack Ange Junior <juniorawounfouet@gmail.com>
2024-06-09 17:49:19 +0200

    initialize page of research

--- app/src/main/java/com/example/blablacar/MainActivity.java
+++ app/src/main/java/com/example/blablacar/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.blablacar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonResearch;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonResearch = findViewById(R.id.buttonResearch);
+
+        buttonResearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Research.class));
+                finish();
+            }
+        });
 
 
     }
